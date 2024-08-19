08749640c3671baf93dfcb8cc350572dcc6e1684
andr33sdev <andr33sdev@gmail.com>
2024-08-19 20:54:09 -0300

    Optimization in header transition responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,8 @@ const Header = () => {
 
     return (
         <Router>
-
             {/* Contenedor Header padre */}
-            <div className={isActiveDropdown ? 'block pt-3 uppercase text-gray-900 font-semibold shadow-md h-fit' : 'flex pt-3 uppercase text-gray-900 font-semibold shadow-md h-24'}>
+            <div className={isActiveDropdown ? 'block uppercase text-gray-900 font-semibold shadow-md h-fit' : 'block uppercase text-gray-900 font-semibold shadow-md h-24'}>
                 {/* Boton menú/x ocultos en medidas medianas-grandes */}
                 <div className='sm:hidden px-5 py-5'>
                     {
@@ -25,10 +24,9 @@ const Header = () => {
                             :
                             <CloseIcon className='cursor-pointer hover:text-red-600 transition-all duration-300' onClick={() => setIsActiveDropdown(!isActiveDropdown)} />
                     }
-
                 </div>
                 {/* Menú tanto para mobile como para desktop */}
-                <div className={`${isActiveDropdown ? 'space-y-3 py-5' : '-translate-x-96 sm:translate-x-0 sm:scale-y-100 sm:mx-auto sm:py-10'} flex flex-col sm:flex-row transition-all items-center justify-center`}>
+                <div className={`${isActiveDropdown ? 'space-y-3 pb-5' : 'opacity-0 sm:opacity-100 justify-start sm:py-10'} flex flex-col sm:flex-row sm:items-center sm:justify-center transition-all ease-in-out duration-300`}>
                     <Link to="/" className='mx-5 hover:text-red-600 transition-all duration-300'>Inicio</Link>
                     <Link to="/about" className='mx-5 hover:text-red-600 transition-all duration-300'>Sobre mi</Link>
                     <Link className='mx-5 hover:text-red-600 transition-all duration-300'>Portfolio</Link>
